13a947ee32d4de4ea33dcc29c3dedd50d8b38d71
thekishanjangid <kishanjangidofficial@gmail.com>
2026-01-20 11:10:43 +0530

    change some button name

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -32,7 +32,7 @@ export function Navbar() {
         </div>
 
         <div className="flex items-center gap-4">
-          <Button size="sm">Start for free</Button>
+          <Button size="sm">Download now</Button>
         </div>
       </Container>
     </nav>
